5478fe1f4dddba468875d32385415477b4d83ed6
manik8 <maniksharma2014.ms@gmail.com>
2020-08-05 22:35:03 +0530

    Preparing for deployement

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
 
 //Connect Database
 connectDB();
 
 app.use(express.json({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.send("API Running Successfull");
-});
 
 // Define Routes
 
@@ -21,8 +18,14 @@ app.use("/api/profile", require("./routes/api/profile"));
 
 app.use("/api/posts", require("./routes/api/posts"));
 
-const PORT = process.env.PORT || 5000;
+// serve static assets and production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-console.log(PORT);
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Started on PORT ${PORT}`));
